07c349ac304d5fb04651fa228bf73a34d1c525f3
Ali Cinar <ali.cinar@outlook.at>
2022-04-05 12:54:29 +0200

    WIP: Added SearchInvoiceServiceImpl to RMIFactoryImpl.
    Co-authored-by: Zikrihan <zikrihan.sahin@students.fhv.at>

--- src/main/java/at/fhv/teame/rmi/RMIFactoryImpl.java
+++ src/main/java/at/fhv/teame/rmi/RMIFactoryImpl.java
@@ -1,8 +1,10 @@
 package at.fhv.teame.rmi;
 
 import at.fhv.teame.application.impl.PurchaseSoundCarrierServiceImpl;
+import at.fhv.teame.application.impl.SearchInvoiceServiceImpl;
 import at.fhv.teame.application.impl.SearchSoundCarrierServiceImpl;
 import at.fhv.teame.sharedlib.rmi.PurchaseSoundCarrierService;
+import at.fhv.teame.sharedlib.rmi.SearchInvoiceService;
 import at.fhv.teame.sharedlib.rmi.SearchSoundCarrierService;
 import at.fhv.teame.sharedlib.rmi.factory.RMIFactory;
 
@@ -25,5 +27,8 @@ public class RMIFactoryImpl extends UnicastRemoteObject implements RMIFactory {
         return new PurchaseSoundCarrierServiceImpl();
     }
 
-
+    @Override
+    public SearchInvoiceService createSearchInvoiceServiceImpl() throws RemoteException {
+        return new SearchInvoiceServiceImpl();
+    }
 }
