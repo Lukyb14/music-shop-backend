a3081e531e2fdbd56acf3c1c39553da72f01dec4
Lukas <lbo3144@students.fhv.at>
2022-04-26 17:07:42 +0200

    Added more integration tests

--- src/test/java/at/fhv/teame/integration/IntegrationTest.java
+++ src/test/java/at/fhv/teame/integration/IntegrationTest.java
@@ -1,37 +1,59 @@
 package at.fhv.teame.integration;
 
 import at.fhv.teame.application.impl.*;
-import at.fhv.teame.sharedlib.dto.*;
+import at.fhv.teame.domain.model.invoice.Invoice;
+import at.fhv.teame.domain.model.soundcarrier.SoundCarrier;
+import at.fhv.teame.domain.repositories.InvoiceRepository;
+import at.fhv.teame.domain.repositories.SoundCarrierRepository;
+import at.fhv.teame.domain.repositories.UserRepository;
+import at.fhv.teame.infrastructure.HibernateInvoiceRepository;
+import at.fhv.teame.infrastructure.HibernateSoundCarrierRepository;
+import at.fhv.teame.infrastructure.HibernateUserRepository;
+import at.fhv.teame.rmi.RMIFactoryImpl;
+import at.fhv.teame.sharedlib.dto.CustomerDTO;
+import at.fhv.teame.sharedlib.dto.MessageDTO;
+import at.fhv.teame.sharedlib.dto.SessionDTO;
+import at.fhv.teame.sharedlib.dto.SoundCarrierDTO;
 import at.fhv.teame.sharedlib.rmi.*;
 import at.fhv.teame.sharedlib.rmi.exceptions.*;
+import at.fhv.teame.sharedlib.rmi.factory.RMIFactory;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.rmi.RemoteException;
-import java.util.*;
+import java.util.List;
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class IntegrationTest {
+class IntegrationTest {
     private static AuthenticationService authenticationService;
     private static MessageService messagingService;
     private static PurchaseSoundCarrierService purchaseSoundCarrierService;
     private static SearchCustomerService searchCustomerService;
     private static SearchInvoiceService searchInvoiceService;
-    private static SearchSoundCarrierServiceImpl soundCarrierService;
+    private static SearchSoundCarrierService searchSoundCarrierService;
     private static WithdrawSoundCarrierService withdrawSoundCarrierService;
+
+    private static SoundCarrierRepository soundCarrierRepository;
+    private static InvoiceRepository invoiceRepository;
+    private static UserRepository userRepository;
 
     @BeforeAll
     static void setup() throws IOException {
-        authenticationService = new AuthenticationServiceImpl();
-        messagingService = new MessagingServiceImpl();
-        purchaseSoundCarrierService = new PurchaseSoundCarrierServiceImpl();
-        searchCustomerService = new SearchCustomerServiceImpl();
-        searchInvoiceService = new SearchInvoiceServiceImpl();
-        soundCarrierService = new SearchSoundCarrierServiceImpl();
-        withdrawSoundCarrierService = new WithdrawalSoundCarrierServiceImpl();
+        RMIFactory rmiFactory = new RMIFactoryImpl();
+        authenticationService = rmiFactory.createSearchAuthenticationServiceImpl();
+        messagingService = rmiFactory.createMessageServiceImpl();
+        purchaseSoundCarrierService = rmiFactory.createPurchaseSoundCarrierServiceImpl();
+        searchCustomerService = rmiFactory.createSearchCustomerServiceImpl();
+        searchInvoiceService = rmiFactory.createSearchInvoiceServiceImpl();
+        searchSoundCarrierService = rmiFactory.createSearchSoundCarrierServiceImpl();
+        withdrawSoundCarrierService = rmiFactory.createWithdrawSoundCarrierServiceImpl();
+
+        soundCarrierRepository = new HibernateSoundCarrierRepository();
+        invoiceRepository = new HibernateInvoiceRepository();
+        userRepository = new HibernateUserRepository();
     }
 
     @Test
@@ -39,7 +61,7 @@ public class IntegrationTest {
 
         SessionDTO sessionDTO = authenticationService.login("aci9089", "PssWrd");
 
-        List<SoundCarrierDTO> soundCarrierDTOs = soundCarrierService.soundCarriersByArtistName("Cofer Brothers", 1, sessionDTO.getSessionId());
+        List<SoundCarrierDTO> soundCarrierDTOs = searchSoundCarrierService.soundCarriersByArtistName("Cofer Brothers", 1, sessionDTO.getSessionId());
 
         for (SoundCarrierDTO soundCarrierDTO : soundCarrierDTOs) {
             System.out.println(soundCarrierDTO.getAlbumName());
@@ -51,7 +73,7 @@ public class IntegrationTest {
 
         SessionDTO sessionDTO = authenticationService.login("aci9089", "PssWrd");
 
-        Integer nrOfSoundCarriers = soundCarrierService.totResultsByArtistName("AC/DC", sessionDTO.getSessionId());
+        int nrOfSoundCarriers = searchSoundCarrierService.totResultsByArtistName("AC/DC", sessionDTO.getSessionId());
         System.out.println("count " + nrOfSoundCarriers);
     }
 
@@ -60,7 +82,7 @@ public class IntegrationTest {
 
         SessionDTO sessionDTO = authenticationService.login("aci9089", "PssWrd");
 
-        Integer nrOfSoundCarriers = soundCarrierService.totResultsBySongName("Back In Black", sessionDTO.getSessionId());
+        int nrOfSoundCarriers = searchSoundCarrierService.totResultsBySongName("Back In Black", sessionDTO.getSessionId());
         System.out.println("count " + nrOfSoundCarriers);
     }
 
@@ -71,7 +93,7 @@ public class IntegrationTest {
 
         SessionDTO sessionDTO = authenticationService.login("aci9089", "PssWrd");
 
-        List<SoundCarrierDTO> soundCarrierDTOS = soundCarrierService.soundCarriersByAlbumName("Beat Break #4", 1, sessionDTO.getSessionId());
+        List<SoundCarrierDTO> soundCarrierDTOS = searchSoundCarrierService.soundCarriersByAlbumName("Beat Break #4", 1, sessionDTO.getSessionId());
 
         for (SoundCarrierDTO soundCarrierDTO : soundCarrierDTOS) {
             System.out.println(soundCarrierDTO.getAlbumName());
@@ -139,28 +161,110 @@ public class IntegrationTest {
     }
 
     @Test
-    void publishMessage() throws LoginFailedException, RemoteException, InvalidSessionException, ReceiveFailedException, PublishingFailedException {
+    void publishMessage() throws LoginFailedException, RemoteException, InvalidSessionException, ReceiveFailedException, PublishingFailedException, InterruptedException {
         SessionDTO sessionDTO = authenticationService.login("lbo3144", "PssWrd");
 
-        List<MessageDTO> messagesBefore = messagingService.fetchMessages(sessionDTO.getSessionId());
+        String randomValue = UUID.randomUUID().toString();
+        messagingService.publishMessage(new MessageDTO("Rock", "New Concert", randomValue), sessionDTO.getSessionId());
 
-        messagingService.publishMessage(new MessageDTO("Rock", "New Concert", "Testing publishMessage()"), sessionDTO.getSessionId());
+        // wait 1 second for ActiveMQ to update itself
+        Thread.sleep(1000);
 
         List<MessageDTO> messagesAfter = messagingService.fetchMessages(sessionDTO.getSessionId());
+        boolean isMessagePresent = false;
+        for (MessageDTO message : messagesAfter) {
+            if (message.getContent().equals(randomValue)) {
+                isMessagePresent = true;
+                break;
+            }
+        }
 
-        assertTrue(messagesBefore.size() < messagesAfter.size());
+        assertTrue(isMessagePresent);
     }
 
     @Test
-    void deleteMessage() throws LoginFailedException, RemoteException, InvalidSessionException, ReceiveFailedException, DeletionFailedException {
+    void deleteMessage() throws LoginFailedException, RemoteException, InvalidSessionException, ReceiveFailedException, DeletionFailedException, InterruptedException {
         SessionDTO sessionDTO = authenticationService.login("lbo3144", "PssWrd");
 
-        List<MessageDTO> messagesBefore = messagingService.fetchMessages(sessionDTO.getSessionId());
+        List<MessageDTO> messagesBeforeDeletion = messagingService.fetchMessages(sessionDTO.getSessionId());
+        String deletedMessageId = messagesBeforeDeletion.get(0).getId();
+        messagingService.deleteMessage(deletedMessageId, sessionDTO.getSessionId());
 
-        messagingService.deleteMessage(messagesBefore.get(0).getId(), sessionDTO.getSessionId());
+        // wait 4 seconds for ActiveMQ to update itself
+        Thread.sleep(4000);
 
-        List<MessageDTO> messagesAfter = messagingService.fetchMessages(sessionDTO.getSessionId());
+        List<MessageDTO> messagesAfterDeletion = messagingService.fetchMessages(sessionDTO.getSessionId());
+        boolean isMessagePresent = false;
+        for (MessageDTO message : messagesAfterDeletion) {
+            if (message.getId().equals(deletedMessageId)) {
+                isMessagePresent = true;
+                break;
+            }
+        }
 
-        assertTrue(messagesBefore.size() > messagesAfter.size());
+        assertFalse(isMessagePresent);
+    }
+
+    @Test
+    void invalidSessionAfterLogout() throws LoginFailedException, RemoteException {
+        SessionDTO sessionDTO = authenticationService.login("lbo3144", "PssWrd");
+
+        authenticationService.logout(sessionDTO.getSessionId());
+
+        assertThrows(InvalidSessionException.class, () -> searchCustomerService.totResultsByFamilyName("Hanf", sessionDTO.getSessionId()));
+    }
+
+    @Test
+    void allTopics() throws LoginFailedException, RemoteException, InvalidSessionException {
+        List<String> expectedTopics = List.of("Pop", "Rock", "System.Message");
+
+        SessionDTO sessionDTO = authenticationService.login("lbo3144", "PssWrd");
+
+        List<String> actualTopics = messagingService.allTopics(sessionDTO.getSessionId());
+
+        assertEquals(expectedTopics, actualTopics);
+    }
+
+    // infrastructure
+
+    @Test
+    void totResultsByAlbumName() {
+        Long results = soundCarrierRepository.totResultsByAlbumName("Back in Black");
+
+        assertTrue(results > 0);
+    }
+
+    @Test
+    void soundCarriersBySongName() {
+        List<SoundCarrier> soundCarriers = soundCarrierRepository.soundCarriersBySongName("JUMP", 1);
+
+        soundCarriers.forEach(sc -> assertTrue(sc.getAlbum().getSongs().stream().anyMatch(song -> song.getTitle().equals("JUMP"))));
+    }
+
+    @Test
+    void soundCarrierByArticleId() {
+        SoundCarrier soundCarrier = soundCarrierRepository.soundCarrierByArticleId("1000");
+
+        assertEquals("1000", soundCarrier.getArticleId());
+    }
+
+    @Test
+    void fillStock() {
+        SoundCarrier soundCarrierBeforeFill = soundCarrierRepository.soundCarrierByArticleId("1000");
+        int expectedStock = soundCarrierBeforeFill.getStock() + 1;
+
+        soundCarrierRepository.fillStock("1000", 1);
+
+        SoundCarrier soundCarrierAfterFill = soundCarrierRepository.soundCarrierByArticleId("1000");
+        int actualStock = soundCarrierAfterFill.getStock();
+
+        assertEquals(expectedStock, actualStock);
+    }
+
+    @Test
+    void invoiceById() {
+        Invoice invoice = invoiceRepository.invoiceById(20000L);
+
+        assertEquals(20000L, invoice.getInvoiceId());
     }
 }
