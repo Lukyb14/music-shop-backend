3afb3fb7a73c8ca2f6cb21c577678158d233bff8
Ali Cinar <ali.cinar@outlook.at>
2022-05-19 12:28:23 +0200

    Implemented and tested SearchDigitalSongService and DigitalSongRepository. Updated Shared Library.

---
+++ src/test/java/at/fhv/teame/application/impl/SearchDigitalSongServiceTest.java
@@ -0,0 +1,115 @@
+package at.fhv.teame.application.impl;
+
+import at.fhv.teame.domain.model.onlineshop.DigitalSong;
+import at.fhv.teame.mocks.MockDigitalSongRepository;
+import at.fhv.teame.sharedlib.dto.DigitalSongDTO;
+import at.fhv.teame.sharedlib.ejb.SearchDigitalSongServiceRemote;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class SearchDigitalSongServiceTest {
+    private SearchDigitalSongServiceRemote searchDigitalSongService;
+
+    @BeforeEach
+    void beforeEach() {
+        searchDigitalSongService = new SearchDigitalSongServiceImpl(new MockDigitalSongRepository());
+    }
+
+    @Test
+    void given_song_title_when_search_digital_song_by_artist_then_expected_song_equals() {
+        // given
+        List<DigitalSong> digitalSongs = createDigitalSongLstDummy();
+
+        List<DigitalSongDTO> digitalSongDtosExpected = new ArrayList<>();
+        for (DigitalSong ds : digitalSongs) {
+            DigitalSongDTO digitalSongDTO = DigitalSongDTO.builder()
+                    .withDigitalSongEntity(ds.getArtist(), ds.getTitle(), ds.getGenre(), ds.getDuration(), ds.getReleaseDate().toString())
+                    .build();
+            digitalSongDtosExpected.add(digitalSongDTO);
+        }
+        // when
+        List<DigitalSongDTO> digitalSongDtosActual = searchDigitalSongService.digitalSongByArtist("Eminem");
+
+        // then
+        for (DigitalSongDTO ds : digitalSongDtosActual) {
+            assertTrue(digitalSongDtosExpected.contains(ds));
+        }
+    }
+
+    @Test
+    void given_song_title_when_search_digital_song_by_genre_then_expected_song_equals() {
+        // given
+        List<DigitalSong> digitalSongs = createDigitalSongLstDummy();
+
+        List<DigitalSongDTO> digitalSongDtosExpected = new ArrayList<>();
+        for (DigitalSong ds : digitalSongs) {
+            DigitalSongDTO digitalSongDTO = DigitalSongDTO.builder()
+                    .withDigitalSongEntity(ds.getArtist(), ds.getTitle(), ds.getGenre(), ds.getDuration(), ds.getReleaseDate().toString())
+                    .build();
+            digitalSongDtosExpected.add(digitalSongDTO);
+        }
+        // when
+        List<DigitalSongDTO> digitalSongDtosActual = searchDigitalSongService.digitalSongByGenre("Hip-Hop");
+
+        // then
+        for (DigitalSongDTO ds : digitalSongDtosActual) {
+            assertTrue(digitalSongDtosExpected.contains(ds));
+        }
+    }
+
+    @Test
+    void given_song_title_when_search_digital_song_by_title_then_expected_song_equals() {
+        // given
+        List<DigitalSong> digitalSongs = createDigitalSongLstDummy();
+
+        List<DigitalSongDTO> digitalSongDtosExpected = new ArrayList<>();
+        for (DigitalSong ds : digitalSongs) {
+            DigitalSongDTO digitalSongDTO = DigitalSongDTO.builder()
+                    .withDigitalSongEntity(ds.getArtist(), ds.getTitle(), ds.getGenre(), ds.getDuration(), ds.getReleaseDate().toString())
+                    .build();
+            digitalSongDtosExpected.add(digitalSongDTO);
+        }
+        // when
+        List<DigitalSongDTO> digitalSongDtosActual = searchDigitalSongService.digitalSongByTitle("8 Mile");
+
+        // then
+        for (DigitalSongDTO ds : digitalSongDtosActual) {
+            assertTrue(digitalSongDtosExpected.contains(ds));
+        }
+    }
+
+    public List<DigitalSong> createDigitalSongLstDummy() {
+        return Arrays.asList(
+                new DigitalSong("Eminem",
+                        "8 Mile",
+                        "Hip-Hop",
+                        "5:30",
+                        LocalDate.of(2000, 1, 1),
+                        "/songs/8 Mile.mp3",
+                        "/covers/8 Mile.jpg"
+                ),
+                new DigitalSong("Eminem",
+                        "Not Afraid",
+                        "Hip-Hop",
+                        "6:30",
+                        LocalDate.of(2011, 3, 1),
+                        "/songs/Not Afraid.mp3",
+                        "/covers/Not Afraid.jpg"
+                ),new DigitalSong("Eminem",
+                        "Till I Collapse",
+                        "Hip-Hop",
+                        "5:50",
+                        LocalDate.of(2003, 4, 1),
+                        "/songs/Till I Collapse.mp3",
+                        "/covers/Till I Collapse.jpg"
+                )
+        );
+    }
+}
