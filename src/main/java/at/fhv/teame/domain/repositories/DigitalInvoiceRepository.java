a18f6208d3bae1a507a2376cee5b4eb18eb0f8f2
Hüseyin Arziman <hueseyin.arziman@gmail.com>
2022-06-02 17:25:56 +0200

    check for already purchased digital song

--- src/main/java/at/fhv/teame/domain/repositories/DigitalInvoiceRepository.java
+++ src/main/java/at/fhv/teame/domain/repositories/DigitalInvoiceRepository.java
@@ -3,8 +3,11 @@ package at.fhv.teame.domain.repositories;
 import at.fhv.teame.domain.model.onlineshop.DigitalInvoice;
 
 import javax.ejb.Local;
+import java.util.List;
 
 @Local
 public interface DigitalInvoiceRepository {
     void store(DigitalInvoice digitalInvoice);
+
+    List<DigitalInvoice> digitalInvoicesByEmail(String email);
 }
